1c1dcd4bddb17fac695ec3b163eb88035604ef07
SapnaS01 <87744431+SapnaS01@users.noreply.github.com>
2025-06-02 16:13:14 +0530

    initial commit

---
+++ src/com/self/oops/Employee.java
@@ -0,0 +1,9 @@
+package com.self.oops;
+
+public class Employee extends Human {
+	public int y=90;
+	public void emp() {
+		System.out.println("Employee Class");
+	}
+
+}
---
+++ src/com/self/oops/Human.java
@@ -0,0 +1,11 @@
+package com.self.oops;
+
+public class Human {
+	
+	public int x=10;
+	
+	public void human() {
+		System.out.println("Human Class");
+	}
+
+}
